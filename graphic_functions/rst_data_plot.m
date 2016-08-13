9e6b98ea0d664ee5b59d7b3bd2b2dac2794702be
Cyril Pernet <wamcyril@gmail.com>
2016-08-13 21:59:56 +0100

    tidy up for speed

--- graphic_functions/rst_data_plot.m
+++ graphic_functions/rst_data_plot.m
@@ -4,7 +4,7 @@ function [est,HDI]=rst_data_plot(Data,varargin)
 % distribution and a summary statistics estimator with 95&% Bayes boot HDI
 %
 % FORMAT: [est,HDI]= rst_data_plot(Data,options)
-%         [est,HDI]= rst_data+plot(Data,'between',0.25,'within',0.025,'pointsize',50,'estimator','median') 
+%         [est,HDI]= rst_data_plot(Data,'between',0.25,'within',0.025,'pointsize',50,'estimator','median') 
 %
 % INPUT: Data is a matrix, data are plotted colmun-wise
 %        options are 
@@ -14,6 +14,8 @@ function [est,HDI]=rst_data_plot(Data,varargin)
 %                'estimator' can be 'median', 'mean', 'trimmed mean' 
 % OUTPUT: est is the summary statistics
 %         HDI the 95% High Density Interval (Bayes bootstrap)
+%         Ouliers is a binary matrix indicating S-outliers
+%         KDE is the hernel density estimated (returns bins and frequencies)
 %
 % example: Data = [randn(100,1) randg(1,100,1) randn(100,1) 1-randg(1,100,1)];
 %          Data(50:55,1) = NaN; Data(90:100,2) = NaN;
@@ -21,7 +23,7 @@ function [est,HDI]=rst_data_plot(Data,varargin)
 %
 % see also cubehelixmap, rst_outlier, rst_RASH, rst_trimmean, rst_hd
 %
-% Cyril Pernet - the university of Edinburgh
+% Cyril Pernet - The University of Edinburgh
 % -------------------------------------------------------------------------
 % Copyright (C) RST Toolbox Team 2015
 
@@ -60,12 +62,47 @@ for n=1:(nargin-1)
    end
 end
 
-%% start
-figure; hold on
-
 %% how many groups
 grouping = size(Data,2);
 gp_index = 1:(1+between_gp_dispersion):(grouping*(1+between_gp_dispersion));
+
+%% Summary stat 
+if strcmpi(estimator,'Mean')
+    est = nanmean(Data,1);
+elseif strcmpi(estimator,'Trimmed mean')
+    est = rst_trimmean(Data,trimming); 
+elseif strcmpi(estimator,'Median')
+    est = rst_hd(Data,decile); % Median estimated using the 5th decile of Harell Davis
+end
+
+%% Bayes bootstrap
+
+% sample with replcaement from Dirichlet
+% sampling = number of observations, e.g. participants
+n=size(Data,1); 
+bb = zeros(Nb,grouping);
+for boot=1:Nb % bootstrap loop
+    theta = exprnd(1,[n,1]);
+    weigths = theta ./ repmat(sum(theta),n,1);
+    resample = (datasample(Data,n,'Replace',true,'Weights',weigths));
+    
+    % compute the estimator
+    if strcmpi(estimator,'Mean')
+        bb(boot,:) = nanmean(resample,1);
+    elseif strcmpi(estimator,'Trimmed Mean')
+        bb(boot,:) = rst_trimmean(resample,20); 
+    elseif strcmpi(estimator,'Median')
+        bb(boot,:) = rst_hd(resample,.5); 
+    end
+end
+
+sorted_data = sort(bb,1); % sort bootstrap estimates
+upper_centile = floor(prob_coverage*size(sorted_data,1)); % upper bound
+nCIs = size(sorted_data,1) - upper_centile;
+HDI = zeros(2,grouping);
+
+%% start
+figure; hold on
 
 %% select color scheme
 color_scheme = cubehelixmap('semi_continuous',grouping+1);
@@ -75,10 +112,7 @@ color_scheme = cubehelixmap('semi_continuous',grouping+1);
 
 for u=1:grouping
     tmp = sort(Data(~isnan(Data(:,u)),u));
-    % find a spread needed
-    s = sum(diff(tmp) < 0.1);
-    spread = [0:(1/s):1];
-    % creater a matrix with that spread
+    % creater a matrix with spread = 2
     change = find(diff(tmp) < 0.1);
     Y = NaN(length(tmp),2);
     Y(1:change(1),1) = tmp(1:change(1));
@@ -102,63 +136,26 @@ for u=1:grouping
         scatter(X(:,p),Y(:,p),point_size,color_scheme(u,:));
         scatter(X(outliers,p),Y(outliers,p),point_size,color_scheme(u,:),'filled');
     end
-end   
-cst = max(abs(diff(Data(:)))) * 0.1;
-axis([0.3 gp_index(grouping)+0.7 min(Data(:))-cst max(Data(:))+cst])    
     
-%% Add the density estimate 
-for u=1:grouping
-    tmp = sort(Data(~isnan(Data(:,u)),u));
+    %% Add the density estimate 
     % get the kernel
     [bc,K]=rst_RASH(tmp,100,dist_method);
     % remove 0s
-    bc(find(K==0)) = [];
-    K(find(K==0))= [];
+    bc(K==0) = [];
+    K(K==0)= [];
     % create symmetric values 
     K = K/max(K); high=(K/2); low=(-high);
     % plot
     y1 = plot(high+gp_index(u),bc); set(y1,'Color',color_scheme(u,:)); hold on
     y2 = plot(low+gp_index(u),bc); set(y1,'Color',color_scheme(u,:)); 
+    if isnumeric(y1)
+        y1 = get(y1); y2 = get(y2); % old school matlab figures
+    end
     xpoints=[y2.XData',y1.XData']; filled=[y2.YData',y1.YData'];
     hold on; fillhandle=fill(xpoints,filled,color_scheme(u,:));
     set(fillhandle,'EdgeColor',color_scheme(u,:),'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color   
-end
 
-%% Add the summary stat with 95% HDI (Bayes bootstrap)
-if strcmpi(estimator,'Mean')
-    est = nanmean(Data,1);
-elseif strcmpi(estimator,'Trimmed mean')
-    est = rst_tm(Y,trimming); 
-elseif strcmpi(estimator,'Median')
-    est = rst_hd(Data,decile); % Median estimated using the 5th decile of Harell Davis
-end
-
-
-% sample with replcaement from Dirichlet
-% sampling = number of observations, e.g. participants
-n=size(Data,1); 
-bb = zeros(Nb,grouping);
-for boot=1:Nb % bootstrap loop
-    theta = exprnd(1,[n,1]);
-    weigths = theta ./ repmat(sum(theta),n,1);
-    resample = (datasample(Data,n,'Replace',true,'Weights',weigths));
-    
-    % compute the estimator
-    if strcmpi(estimator,'Mean')
-        bb(boot,:) = nanmean(resample,1);
-    elseif strcmpi(estimator,'Trimmed Mean')
-        bb(boot,:) = rst_tm(resample,20); 
-    elseif strcmpi(estimator,'Median')
-        bb(boot,:) = rst_hd(resample,.5); 
-    end
-end
-
-sorted_data = sort(bb,1); % sort bootstrap estimates
-upper_centile = floor(prob_coverage*size(sorted_data,1)); % upper bound
-nCIs = size(sorted_data,1) - upper_centile;
-HDI = zeros(2,grouping);
-
-for u = 1:grouping
+    %% Hight Density Intervals
     tmp = sorted_data(:,u);
     ci = 1:nCIs; ciWidth = tmp(ci+upper_centile) - tmp(ci); % all centile distances
     [~,index]=find(ciWidth == min(ciWidth)); % densest centile
@@ -167,13 +164,16 @@ for u = 1:grouping
     HDI(2,u) = tmp(index+upper_centile);
     
     % plot this with a rectangle
-    X = [(gp_index(u)-0.3):0.1:(gp_index(u)+0.3)];
+    X = (gp_index(u)-0.3):0.1:(gp_index(u)+0.3);
     plot(X(3:5),repmat(est(u),[1,length(X(3:5))]),'LineWidth',6,'Color',color_scheme(u,:));
     rectangle('Position',[X(3),HDI(1,u),X(5)-X(3),HDI(2,u)-HDI(1,u)],'Curvature',[0.4 0.4],'LineWidth',3,'EdgeColor',color_scheme(u,:))
 end
 
 
 %% finish off
+cst = max(abs(diff(Data(:)))) * 0.1;
+axis([0.3 gp_index(grouping)+0.7 min(Data(:))-cst max(Data(:))+cst])    
+
 if size(Data,1) == 1
 title(sprintf('Data distribution with %s and 95%% High Dentity Interval',estimator));
 else
