84097a059abeb8ab44289ec4084b43b6445219a3
Cyril Pernet <cyril.pernet@ed.ac.uk>
2017-03-13 14:07:54 +0000

    multiple comparions updated
    rst_multicompare and rst_gp_multicompare updated to handle mean, median,
    trimmed mean, nicer figure; some update for multiple testing as well

--- stats_functions/univariate/rst_gp_multicompare.m
+++ stats_functions/univariate/rst_gp_multicompare.m
@@ -1,100 +1,261 @@
-function [difference,CI,h]=rst_gp_multicompare(gp1,gp2,flag)
+function [diff,CI,p,alphav,h]=rst_gp_multicompare(gp1,gp2,varargin)
 
-% Test for mean differences between groups with an adjustment for
-% multiple comparisons using thr max under H0
+% Test for differences between groups with an adjustment for multiple 
+% comparisons - This is not the same as post-hoc testing in ANOVA in which 
+% a significant interaction was found in a 1st place. The control is across
+% tested difference only. The control is across tested difference only. 
+% Multiple comparisons are computed as described in Wilcox, R.R. (2012). 
+% Robust Estimation and Hypothesis Testing (3rd Ed). Elsevier, Academic 
+% Press, San Diego, CA, USA. p329-330 (section 7.4.7).
 %
-% FORMAT [difference,CI,h]=rst_gp_multicompare(gp1,gp2,flag)
+% FORMAT [diff,CI,p,alphav,h]=rst_gp_multicompare(gp1,gp2,'alphav',0.05,'estimator','median','newfig','yes')
 %
-% INPUT gp1 and gp2 are 2 matrices n*p and m*p with p the conditions to 
-%       compare column-wise 
-%       flag - indicates to make a figure or not
+% INPUT gp1 and gp2 are 2 matrices n*p and m*p 
+%                   with p the conditions to compare column-wise 
+%       alphav    = 5% (default) - for n<80 and the number of pairs is more than
+%                   10, only 5% or 10% are allowed
+%       estimator = which estimator to use 'mean','median' (Harell-Davis) or 'trimmed mean'
+%       newfig    = 'yes' or 'no' allows to plot within an existing one 
 %
-% OUTPUT difference is the mean difference
+% OUTPUT difference is the estimator difference
 %        CI the adjusted 95% CI
-%        h hypothesis testing
+%        p        = the percentile bootstrap p values
+%        alphav   = the adjusted alpha value used for significance
+%        note there is no p values when comparing means
+%        h  = significance of the test
+%
+% usage
+% gp1 = randn(30,3); gp2 = randn(30,3); gp2(:,2) = gp2(:,2)+1; % no differences but gp2
+% [diff,CI,p,alphav,h]= rst_gp_multicompare(gp1,gp2,'alphav',0.05,'estimator','trimmed mean','newfig','yes')
+% 
+% The tested hypothesis is that each group diffence Dij = 0. 
+% When testing mean differences, adjustement is based on the maximum 
+% differences over all bootstraps across conditions using a bootstrap t.
+% This is limited to N<80. Note that there is no p values associated.
+% For other estimators, FWE rate is controlled using the SR method 
+% if N<80 and less than 10 pairs are tested. If N<80 and more than 10 pairs
+% are tested, alphav adjusted for the number of tests. If N>80, alphav is 
+% adjusted using Hochberg step-up procedure. Overview of these procedures, 
+% and others, can be found at <http://en.wikipedia.org/wiki/Familywise_error_rate>
 %
 % Cyril Pernet 
 % ----------------------------------------------------------------------
-% Copyright (C) RST Toolbox Team 2014
+% Copyright (C) RST Toolbox Team 2017
 
-nboot = 1000;
-alpha = 5/100;
-if nargin<3
-    flag = 1;
+% extra input for quality check
+% 'plothist' if one wants to plot the density histograms of each difference
+
+% _hard coded_ 
+nboot = 1000; % how many bootstraps are used
+
+% _soft coded_
+alphav   = 5/100; % non adjusted alpha value
+estimator ='median'; % by default use a robust estimator
+newfig    = 'yes'; % start a new figure
+plothist  = 'no'; % do not create additional histograms of differences
+
+% _check inputs_ 
+if any(size(gp1,2) ~= size(gp2,2))
+    error('input data must be of the same size')
+else
+  [n,p] = size(gp1);    
 end
 
-% multiple comparisons between 2 groups
-boot_gp1 = zeros(size(gp1,1),size(gp1,2),nboot); B=1;
-while B~=nboot+1
-    tmp1 = randi(size(gp1,1),1,size(gp1,1))';
-    if length(unique(tmp1)) ~= 1
-        boot_gp1(:,:,B) = gp1(tmp1,:);
-        B=B+1;
+% check options
+for in=1:length(varargin)
+    if strcmpi(varargin(in),'alphav')
+        alphav = cell2mat(varargin(in+1));
+    elseif strcmpi(varargin(in),'estimator')
+        if ~strcmpi(varargin(in+1),'median') && ...
+                ~strcmpi(varargin(in+1),'mean') && ...
+                ~strcmpi(varargin(in+1),'trimmed mean')
+            error(['estimator ' cell2mat(varargin(in+1)) ' is not recognized'])
+        else
+            estimator = cell2mat(varargin(in+1));
+        end
+    elseif strcmpi(varargin(in),'newfig')
+        newfig = cell2mat(varargin(in+1));
+    elseif strcmpi(varargin(in),'plothist')
+        plothist = cell2mat(varargin(in+1));
     end
 end
+orig_alpha = alphav;
+low = round(nboot.*orig_alpha./2);
+high = nboot - low;
 
-boot_gp2 = zeros(size(gp2,1),size(gp2,2),nboot); B=1;
-while B~=nboot+1
-    tmp2 = randi(size(gp2,1),1,size(gp2,1))';
-    if length(unique(tmp2)) ~= 1
-        boot_gp2(:,:,B) = gp2(tmp2,:);
-        B=B+1;
+%% multiple comparisons between 2 group means
+if strcmpi(estimator,'mean')
+    
+    if length(gp1)>80 && length(gp2) > 80
+        disp('adjustment needed for large N not implemented (see Wilcox 2nd edition, p314'); return
+    end
+    
+    boot_gp1 = zeros(size(gp1,1),size(gp1,2),nboot); B=1;
+    while B~=nboot+1
+        tmp1 = randi(size(gp1,1),1,size(gp1,1))';
+        if length(unique(tmp1)) ~= 1
+            boot_gp1(:,:,B) = gp1(tmp1,:);
+            B=B+1;
+        end
+    end
+    
+    boot_gp2 = zeros(size(gp2,1),size(gp2,2),nboot); B=1;
+    while B~=nboot+1
+        tmp2 = randi(size(gp2,1),1,size(gp2,1))';
+        if length(unique(tmp2)) ~= 1
+            boot_gp2(:,:,B) = gp2(tmp2,:);
+            B=B+1;
+        end
+    end
+    
+    means_gp1 = squeeze(nanmean(boot_gp1,1)); % means over resampled subjects
+    means_gp2 = squeeze(nanmean(boot_gp2,1));
+    boot_means1 = nanmean(means_gp1,2); % means over all bootstrap
+    boot_means2 = nanmean(means_gp2,2);
+    Tau1 = (1/(nboot-1))*nansum((means_gp1-boot_means1(:,ones(1,nboot))).^2,2); % standard error of each condition
+    Tau2 = (1/(nboot-1))*nansum((means_gp2-boot_means2(:,ones(1,nboot))).^2,2);
+    H = abs(means_gp1 - means_gp2 - repmat((boot_means1-boot_means2),1,nboot)) ...
+        ./ repmat((sqrt(Tau1+Tau2)),1,nboot); % difference between conditions
+    Hmax = sort(max(H)); % maximum differences over all bootstraps across conditions
+    
+    u = round((1-alphav)*nboot);
+    diff = nanmean(gp1,1)' - nanmean(gp2,1)';
+    bound = Hmax(u).*(sqrt(Tau1+Tau2)); % because of Hmax, this adjusts for multiple testing
+    CI = NaN(length(diff),2);
+    CI(:,1) = diff - bound;
+    CI(:,2) = diff + bound;
+    h = single(CI(:,1) > 0) + single(CI(:,2) < 0);
+    p = NaN; CI = CI';
+end
+
+
+%% multiple comparisons between 2 group medians or trimmed means
+if ~strcmpi(estimator,'mean')
+    
+    % adjusted alpha value
+    if n < 80 && p<=10 % small sample size, method SR
+        if sum([alphav ~= .05 alphav ~= .1]) == 2
+            error('multiple comparisons with N<80 and less than 10 pairs, only works with apha 5% or 10%');
+        else
+            tmp = nan(1,p);
+            for t=1:p
+                tmp(t) = getalphavc(alphav,t); % see sub-function getalphav
+            end
+            alphav = tmp; clear tmp
+        end
+    else % small samples and more than 10 tests or large sample: use Hochberg method
+        alphav = repmat(alphav,1,p) ./ [1:p];
+    end
+    
+    boot_gp1 = zeros(size(gp1,1),size(gp1,2),nboot); B=1;
+    while B~=nboot+1
+        tmp1 = randi(size(gp1,1),1,size(gp1,1))';
+        if length(unique(tmp1)) ~= 1
+            boot_gp1(:,:,B) = gp1(tmp1,:);
+            B=B+1;
+        end
+    end
+    
+    boot_gp2 = zeros(size(gp2,1),size(gp2,2),nboot); B=1;
+    while B~=nboot+1
+        tmp2 = randi(size(gp2,1),1,size(gp2,1))';
+        if length(unique(tmp2)) ~= 1
+            boot_gp2(:,:,B) = gp2(tmp2,:);
+            B=B+1;
+        end
+    end
+    
+    % compute the difference between estimators
+    D = NaN(p,nboot);
+    CI = NaN(2,p);
+    for t=1:p
+        if strcmpi(estimator,'median')
+            diff(t) = rst_hd(gp1(:,t),0.5) - rst_hd(gp2(:,t),0.5);
+            boot_est1 = rst_hd(squeeze(boot_gp1(:,t,:)),0.5);  % use the Harell David estimator
+            boot_est2 = rst_hd(squeeze(boot_gp2(:,t,:)),0.5);
+        elseif strcmpi(estimator,'trimmed mean')
+            diff(t) = rst_trimmean(gp1(:,t)) - rst_trimmean(gp2(:,t));
+            boot_est1 = rst_trimmean(squeeze(boot_gp1(:,t,:))); % 20% trimmed mean
+            boot_est2 = rst_trimmean(squeeze(boot_gp2(:,t,:)));
+        end
+        D(t,:) = sort(boot_est1 - boot_est2);
+        CI(:,t) = [D(t,low) D(t,high)];
+    end
+    
+    % get the p values and h
+    pl= sum((D>0),2) / nboot;
+    p= 2*(min(pl,1-pl)); % generalized p-value
+    p(p==0) = 1/nboot; % check p-values of 0, change to precision
+    p(sum(D,2)==0) = 1; % check special case of 0 difference for all pairs, change to 1
+    
+    % return h based on adjusted alpha-value
+    L = size(gp1,2); h = zeros(L,1);
+    [sortedp,order]= sort(p','descend');
+    [~,inverseindex] = sort(order);
+    for t=1:L
+        test = sortedp(t) <= alphav(t);
+        if test == 1
+            h(t:L) = 1; break
+        end
+    end
+    h = h(inverseindex);
+end
+
+
+%% figure
+if strcmpi(newfig,'yes')
+    figure('Name', 'group comparisons'); L = size(gp1,2); D = gp1 - gp2;
+    set(gcf,'Color','w'); color_scheme = cubehelixmap('semi_continuous',L+10);
+    for i=1:L
+        rectangle('Position',[i-0.2,CI(1,i),0.4,CI(2,i)-CI(1,i)],'Curvature',[0.4 0.4],'LineWidth',2,...
+            'FaceColor',color_scheme(i+8,:),'EdgeColor',[0.35 0.35 0.35]); hold on;
+        plot([i-0.2 i+0.2],[diff(i) diff(i)],'LineWidth',3,'Color',[0.35 0.35 0.35]);
+        tmp = repmat(D(:,i),1,2); tmp(1:2:end,1) = NaN; tmp(2:2:end,2) = NaN; 
+        scatter(repmat(i-0.05,n,1),tmp(:,1),50,'k','Filled');
+        scatter(repmat(i+0.05,n,1),tmp(:,2),50,'k','Filled');
+    end
+    if strcmpi(estimator,'mean')
+        title('Mean differences between groups \n with adjusted CI','FontSize',16); 
+    else
+        title(sprintf('%s differences between \n groups with %g  CI',estimator,100-orig_alpha*100),'FontSize',16); 
+    end
+
+    ylabel('Difference between groups');box on; grid on
+    xlabel(sprintf('Test %g ',[1:L])); 
+    set(gca,'FontSize',12); v=axis; axis([v(1) v(2) v(3)-0.5 v(4)+0.5]);
+end
+
+end
+
+function alphavc = getalphavc(alphav,L)
+
+% table precomputed by Wilcox p330
+
+if alphav == 5/100
+    if     L == 1;  alphavc = .05;
+    elseif L == 2;  alphavc = .025;
+    elseif L == 3;  alphavc = .0169;
+    elseif L == 4;  alphavc = .0127;
+    elseif L == 5;  alphavc = .0102;
+    elseif L == 6;  alphavc = .00851;
+    elseif L == 7;  alphavc = .0073;
+    elseif L == 8;  alphavc = .00639;
+    elseif L == 9;  alphavc = .00568;
+    elseif L == 10; alphavc = .00511;
+    end
+else
+    if     L == 1;  alphavc = .01;
+    elseif L == 2;  alphavc = .005;
+    elseif L == 3;  alphavc = .00334;
+    elseif L == 4;  alphavc = .00251;
+    elseif L == 5;  alphavc = .00201;
+    elseif L == 6;  alphavc = .00167;
+    elseif L == 7;  alphavc = .00143;
+    elseif L == 8;  alphavc = .00126;
+    elseif L == 9;  alphavc = .00112;
+    elseif L == 10; alphavc = .00101;
     end
 end
-
-means_gp1 = squeeze(nanmean(boot_gp1,1)); % means over resampled subjects
-means_gp2 = squeeze(nanmean(boot_gp2,1));
-boot_means1 = nanmean(means_gp1,2); % means over all bootstrap
-boot_means2 = nanmean(means_gp2,2);
-Tau1 = (1/(nboot-1))*nansum((means_gp1-boot_means1(:,ones(1,nboot))).^2,2); % standard error of each condition
-Tau2 = (1/(nboot-1))*nansum((means_gp2-boot_means2(:,ones(1,nboot))).^2,2);
-H = abs(means_gp1 - means_gp2 - repmat((boot_means1-boot_means2),1,nboot)) ...
-    ./ repmat((sqrt(Tau1+Tau2)),1,nboot); % difference between conditions
-Hmax = sort(max(H)); % maximum differences over all bootstraps across conditions
-
-u = round((1-alpha)*nboot);
-difference = nanmean(gp1,1)' - nanmean(gp2,1)';
-bound = Hmax(u).*(sqrt(Tau1+Tau2)); % because of Hmax, this adjusts for multiple testing
-CI = NaN(length(difference),2);
-CI(:,1) = difference - bound;
-CI(:,2) = difference + bound;
-h = single(CI(:,1) > 0) + single(CI(:,2) < 0); 
-
-% non corrected version would be
-% boot_difference = sort((means_gp1 - means_gp2)');
-% l = round((alpha*nboot)/2); u = nboot-l;
-% CI(:,1) = boot_difference(l+1,:);
-% CI(:,2) = boot_difference(u,:);
-
-% adjustement for large N
-
-if length(gp1)>80 && length(gp2) > 80
-    disp('adjustment needed for large N - to complete Wilcox p314')
 end
-
-% plot
-if flag == 1
-    low = round((1-alpha/2)*nboot);
-    high = nboot - low;
-    figure('Name', 'Pair-wise comparisons')
-    set(gcf,'Color','w')
-    subplot(1,2,1); plot(nanmean(gp1),'LineWidth',2); hold on
-    plot(nanmean(gp2),'r','LineWidth',2); grid on;
-    means_gp1 = sort(means_gp1,2); means_gp2 = sort(means_gp2,2);
-    fillhandle = patch([1:size(gp1,2) size(gp1,2):-1:1], [means_gp1(:,high); flipud(means_gp1(:,low))], [0 0 1]);
-    set(fillhandle,'EdgeColor',[0 0 1],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
-    fillhandle = patch([1:size(gp1,2) size(gp1,2):-1:1], [means_gp2(:,high); flipud(means_gp2(:,low))], [1 0 0]);
-    set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
-    title('Means','FontSize',14); subplot(1,2,2);
-    bar([1:length(difference)],difference); hold on
-    errorbar([1:length(difference)],difference,bound,bound,'r','LineWidth',2);
-    title(['Difference between groups and ' num2str(100-alpha*100) '% CI'],'FontSize',16); grid on
-    ylabel('Difference between groups','FontSize',14);
-    xlabel('Conditions','FontSize',14);
-    set(gca,'FontSize',12);
-    set(gca,'xtick',[1:length(difference)]);
-    v=axis; ymin = v(3) + (v(4)-v(3))/10;
-end
-
 
 
--- stats_functions/univariate/rst_multicompare.m
+++ stats_functions/univariate/rst_multicompare.m
@@ -1,33 +1,44 @@
-function [diff,CI_boot,p,h]= rst_multicompare(Data,Pairs,varargin)
+function [diff,CI,p,alphav,h]= rst_multicompare(Data,Pairs,varargin)
 
-% performs multiple comparisons between pairs based on a percentile bootstrap
-% on differences - alphav is adjusted to control the type 1 error rate.
+% Performs multiple comparisons between pairs (repeated measures) based on 
+% a percentile bootstrap on differences - the alpha value is adjusted to 
+% control the type 1 error rate. This is not the same as post-hoc testing
+% in ANOVA in which significant effect across all conditions was found in
+% a 1st place. The control is across tested difference only. Multiple 
+% comparisons are computed as described in Wilcox, R.R. (2012). Robust 
+% Estimation and Hypothesis Testing (3rd Ed). Elsevier, Academic Press, 
+% San Diego, CA, USA. p398-401 (section 8.3.2).
 %
 % FORMAT
-% [diff,CI_boot,p,h,]= rst_multicompare(Data,Pairs,'alphav',0.05,'estimator','median','newfig','yes')
+% [diff,CI,p,alphav,h]= rst_multicompare(Data,Pairs,'alphav',0.05,'estimator','median','newfig','yes')
 %
 % INPUT
 % Data      = 2D matrix (n*p) of repeated measures
 % pairs     = matrix 2*n for which pairs to test ([] means all)
-% alphav    = 5% (default) or 10%
+% alphav    = 5% (default) - for n<80 and the number of pairs is more than
+%             10, only 5% or 10% are allowed
 % estimator = which estimator to use 'mean','median' (Harell-Davis) or 'trimmed mean'
 % newfig    = 'yes' or 'no' allows to plot within an existing one 
 %
 % OUTPUT
 % diff     = all pairwise differences between conditions
-% CI_boot  = percentile bootstrap confidence intervals of the differences
-% p        = structrure with 
-%           p.adjusted = the adjusted p value used for significance
-%           p.boot     = the percentile bootstrap p values
+% CI       = percentile bootstrap confidence intervals of the differences
+% p        = the percentile bootstrap p values
+% alphav   = the adjusted alpha value used for significance
 % h        = significance of the p value (adjusted for multiple tests)
 %
-% Multiple comparisons are computed as described in Wilcox, R.R. (2005)
-% Robust Estimation and Hypothesis Testing (2nd Ed). Elsevier, Academic
-% Press, San Diego, CA, USA. If N<80 and less than 10 pairs are tested, the
-% alphav value is set a priori based on simulation results. If N<80 and more
-% then 10 pairs are tested, alphav is Bonferroni corrected. If N>80, alphav
-% is adjusted using Hochberg step-up procedure. Overview of these
-% procedures, and others, can be found at 
+% usage
+% Data = randn(30,6); Data(:,5) = Data(:,5)+1; % no differences but column 5
+% Pairs = [1 4; 2 5; 3 6];
+% [diff,CI,p,alphav,h]= rst_multicompare(Data,Pairs,'alphav',0.05,'estimator','trimmed mean','newfig','yes')
+% 
+% The tested hypothesis is that each diffence scores Dij = 0. This is
+% performed by bootstrapping the matrix of differences, computing N estimators
+% and testing how many times it is bigger than 0. FWE rate is controlled 
+% using the SR method if N<80 and less than 10 pairs are tested. If N<80 
+% and more than 10 pairs are tested, alphav adjusted for the number of tests.
+% If N>80, alphav is adjusted using Hochberg step-up procedure. 
+% Overview of these procedures, and others, can be found at 
 % <http://en.wikipedia.org/wiki/Familywise_error_rate>
 %
 % Cyril Pernet
@@ -35,7 +46,7 @@ function [diff,CI_boot,p,h]= rst_multicompare(Data,Pairs,varargin)
 % Copyright (C) RST Toolbox Team 2017
 
 % extra input for quality check
-% 'plothist' if one want to plot the density histograms of each difference
+% 'plothist' if one wants to plot the density histograms of each difference
 
 
 %% set defaults
@@ -44,7 +55,7 @@ function [diff,CI_boot,p,h]= rst_multicompare(Data,Pairs,varargin)
 nboot = 1000; % how many bootstraps are used
 
 % _soft coded_
-alphav   = 5/100; % non adjusted alpha value
+alphav    = 5/100; % non adjusted alpha value
 estimator ='median'; % by default use a robust estimator
 newfig    = 'yes'; % start a new figure
 plothist  = 'no'; % do not create additional histograms of differences
@@ -68,6 +79,7 @@ L = length(all_pairs);
 for in=1:length(varargin)
         if strcmpi(varargin(in),'alphav')
             alphav = cell2mat(varargin(in+1));
+            if alphav > 1; alphav = alphav / 100; end
         elseif strcmpi(varargin(in),'estimator')
             if ~strcmpi(varargin(in+1),'median') && ...
                     ~strcmpi(varargin(in+1),'mean') && ...
@@ -83,29 +95,29 @@ for in=1:length(varargin)
         end
 end
 
-% get the adjusted alpha value
-if n < 80 % small sample size, method SR
-    if L>10
-        alphav = alphav / L;
-    else
-        test(1) = (alphav ~= .05);
-        test(2) = (alphav ~= .1);
-        if sum(test) ==2
-            error('multiple comparisons only works with apha 5% or 10%');
-        else
-            alphav = getalphavc(alphav,L); % see sub-function getalphav 
-        end
-    end
-end
-
-p.adjusted = alphav;
+% non-adjusted confidence intervals
 low = round(nboot.*alphav./2);
 high = nboot - low;
+orig_alpha = alphav;
 
+% get the adjusted alpha value
+if n < 80 && L<=10 % small sample size, method SR
+        if sum([alphav ~= .05 alphav ~= .1]) == 2
+            error('multiple comparisons with N<80 and less than 10 pairs, only works with apha 5% or 10%');
+        else
+            tmp = nan(1,L);
+            for t=1:L
+                tmp(t) = getalphavc(alphav,t); % see sub-function getalphav 
+            end
+            alphav = tmp; clear tmp
+        end
+else % small samples and more than 10 tests or large sample: use Hochberg method
+    alphav = repmat(alphav,1,L) ./ [1:L];
+end
 
 %% start the bootstrap
 
-% compute the differences
+% _compute the differences_  
 D = zeros(size(Data,1),L);
 for i=1:L
     D(:,i) = Data(:,all_pairs(i,1)) - Data(:,all_pairs(i,2));
@@ -119,9 +131,9 @@ elseif strcmpi(estimator,'trimmed mean')
     diff = rst_trimmean(D); % 20% trimmed mean
 end
 
-% _compute bootstrap differences_
+% _compute bootstrap differences_  
 
-% make a boot table with at least 2 different observations
+% make a boot table with at least 2 different observations  
 boot_table = randi(n,[n nboot*2]);
 for B =1:nboot*2
     if length(unique(boot_table(:,B))) < 3 
@@ -131,11 +143,11 @@ end
 boot_table(:,isnan(sum(boot_table,1))) = [];
 boot_table = boot_table(:,1:nboot);
 
-% do all the bootstraps at once
+% _do all the bootstraps at once_  
 avg_boot = zeros(nboot,L); 
-for d=1:L
+for d=1:L % for each pair
     tmp = D(:,d);
-    if strcmpi(estimator,'mean')
+    if strcmpi(estimator,'mean') % compute the nboot estimators
         avg_boot(:,d) = nanmean(tmp(boot_table));
     elseif strcmpi(estimator,'median')
         avg_boot(:,d) = rst_hd(tmp(boot_table),0.5);
@@ -144,42 +156,26 @@ for d=1:L
     end
 end
 avg_boot = sort(avg_boot,1);
-CI_boot = [avg_boot(low,:);avg_boot(high,:)];
+CI       = [avg_boot(low,:);avg_boot(high,:)]; % get the confidence intervals
 
 %% get the p values and h
 
 pl= sum((avg_boot>0)) / nboot;
-p= 2*(min(pl,1-pl));
-% check p-values of 0, change to precision
-p(p==0) = 1/nboot; 
-% check special case of 0 difference for all pairs, change to 1
-p(sum(avg_boot,1)==0) = 1;
+p= 2*(min(pl,1-pl)); % generalized p-value
+p(p==0) = 1/nboot; % check p-values of 0, change to precision
+p(sum(avg_boot,1)==0) = 1; % check special case of 0 difference for all pairs, change to 1
 
 % return h based on adjusted alpha-value
 h = zeros(L,1);  
 [sortedp,order]= sort(p','descend');
 [~,inverseindex] = sort(order);
-if n < 80 % small sample size, method SR
-    tmp = sortedp<alphav;
-    h = tmp(inverseindex);
-    
-else % large sample size, Hochberg, 1988
-    decreased_alphav = repmat(alphav,L,1)./(1:L)';
-    test = sortedp < decreased_alphav;
-    go = 1; index = 1; 
-    while go == 1
-        if test(index) == 1
-            h(index:L) = 1;
-            go = 0;
-        elseif test(index) == 0
-            index = index+1;
-            if index == length(test)
-                go = 0;
-            end
-        end
+for t=1:L
+    test = sortedp(t) <= alphav(t);
+    if test == 1
+        h(t:L) = 1; break
     end
-    h = h(inverseindex);  
 end
+h = h(inverseindex);
 
 %% figures
 
@@ -187,14 +183,14 @@ if strcmpi(newfig,'yes')
     color_scheme = cubehelixmap('semi_continuous',L+10);
     figure('Name', 'Pair-wise comparisons');set(gcf,'Color','w')
     for i=1:L
-        rectangle('Position',[i-0.2,CI_boot(1,i),0.4,CI_boot(2,i)-CI_boot(1,i)],'Curvature',[0.4 0.4],'LineWidth',2,...
+        rectangle('Position',[i-0.2,CI(1,i),0.4,CI(2,i)-CI(1,i)],'Curvature',[0.4 0.4],'LineWidth',2,...
             'FaceColor',color_scheme(i+8,:),'EdgeColor',[0.35 0.35 0.35]); hold on;
         plot([i-0.2 i+0.2],[diff(i) diff(i)],'LineWidth',3,'Color',[0.35 0.35 0.35]);
         tmp = repmat(D(:,i),1,2); tmp(1:2:end,1) = NaN; tmp(2:2:end,2) = NaN; 
         scatter(repmat(i-0.05,n,1),tmp(:,1),50,'k','Filled');
         scatter(repmat(i+0.05,n,1),tmp(:,2),50,'k','Filled');
     end
-    title(sprintf('%s difference between \n pairs with %g %% CI',estimator,100-alphav*100),'FontSize',16); 
+    title(sprintf('%s difference between \n pairs with %g %% CI',estimator,100-orig_alpha*100),'FontSize',16); 
     ylabel('Difference between pairs');
     for i=1:L
         xname{i} = [num2str(all_pairs(i,1)) '/' num2str(all_pairs(i,2)) ' '];
@@ -215,30 +211,30 @@ end
 
 function alphavc = getalphavc(alphav,L)
 
-% table precomputed by Wilcox
+% table precomputed by Wilcox p347
 
 if alphav == 5/100
-    if L == 1; alphavc = .025;
-    elseif L == 2; alphavc = .025;
-    elseif L == 3; alphavc = .0169;
-    elseif L == 4; alphavc = .0127;
-    elseif L == 5; alphavc = .0102;
-    elseif L == 6; alphavc = .00851;
-    elseif L == 7; alphavc = .0073;
-    elseif L == 8; alphavc = .00639;
-    elseif L == 9; alphavc = .00568;
+    if     L == 1;  alphavc = .025;
+    elseif L == 2;  alphavc = .025;
+    elseif L == 3;  alphavc = .0169;
+    elseif L == 4;  alphavc = .0127;
+    elseif L == 5;  alphavc = .0102;
+    elseif L == 6;  alphavc = .00851;
+    elseif L == 7;  alphavc = .0073;
+    elseif L == 8;  alphavc = .00639;
+    elseif L == 9;  alphavc = .00568;
     elseif L == 10; alphavc = .00511;
     end
 else
-    if L == 1; alphavc = .005;
-    elseif L == 2; alphavc = .005;
-    elseif L == 3; alphavc = .00334;
-    elseif L == 4; alphavc = .00251;
-    elseif L == 5; alphavc = .00201;
-    elseif L == 6; alphavc = .00167;
-    elseif L == 7; alphavc = .00143;
-    elseif L == 8; alphavc = .00126;
-    elseif L == 9; alphavc = .00112;
+    if     L == 1;  alphavc = .005;
+    elseif L == 2;  alphavc = .005;
+    elseif L == 3;  alphavc = .00334;
+    elseif L == 4;  alphavc = .00251;
+    elseif L == 5;  alphavc = .00201;
+    elseif L == 6;  alphavc = .00167;
+    elseif L == 7;  alphavc = .00143;
+    elseif L == 8;  alphavc = .00126;
+    elseif L == 9;  alphavc = .00112;
     elseif L == 10; alphavc = .00101;
     end
 end
