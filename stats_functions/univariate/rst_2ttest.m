613956e07c258fa1ecafaf8c1b2cc6c07afe6125
Cyril Pernet <cyril.pernet@ed.ac.uk>
2017-03-20 12:56:29 +0000

    2 samples t-tests updates variables in and figure

--- stats_functions/univariate/rst_2ttest.m
+++ stats_functions/univariate/rst_2ttest.m
@@ -1,121 +1,111 @@
-function [h,M,CI,p] = rst_2ttest(varargin)
+function [h,M,CI,p] = rst_2ttest(gp1,gp2,varargin)
 
 % performs a robust two samples test, ie a percentile bootstrap on the
 % difference of the estimators
 %
 % FORMAT
-% [h,M,CI] = rst_2ttest(X,Y)
-% [h,M,CI,p] = rst_2ttest(X,Y,'estimator',alpha,plot_option,nboot)
+% [h,M,CI]   = rst_2ttest(X,Y)
+% [h,M,CI,p] = rst_2ttest(X,Y,'alphav',0.05,'estimator','median','newfig','yes')
 %
 % INPUTS
-% - X and Y are a n-by-p matrix, if p>1 the test is computed for successive columns 
-% and resampling is identical in each column
-% - estimator = 'mean' or 'median' or 'trimmean'
-% - plot_option indicates to plot the result (1) or not (0 - default)
-% - alpha is the alpha level, default = 5%
-% - nboot is the number of bootstrap, default = 600
+% - X and Y are a n-by-p matrix, if p>1 the test is computed for successive columns
+%   and resampling is identical in each column
+% - alphav is the alpha level, default = 5%
+% - 'estimator' can be 'mean', 'median' or 'trimmed mean'
+% - newfig    = 'yes' or 'no' allows to plot within an existing one
 %
 % OUTPUTS
-% [h,M,CI] = rst_1ttest(X,Y,'mean')
-% h is the hypothesis that the mean is 0 if h = 0 then this is not
-% significantly different from 0 ; note that if data is a matrix, 
-% a Bonferonni correction is applied for the number of tests (columns)
-% M is the mean difference
-% CI are the alpha percent (Bonferoni corrected) confidence intervals used to get h
+% h  is the hypothesis that the mean is 0 if h = 0 then this is not
+%    significantly different from 0 ; note that if data is a matrix,
+%    a Bonferonni correction is applied for the number of tests (columns)
+% M  is the estimator difference
+% CI is the alpha percent (Bonferoni corrected) confidence intervals used to get h
 %
-% [h,M,CI,p] = rst_1ttest(X,Y,'median');
-% [h,M,CI,p] = rst_1ttest(X,Y,'trimmean');
-% h is the hypothesis that the median or 20% trimmean is 0 if h = 0 then this is not
-% significantly different from 0 ; note that if data is a matrix, 
-% a Bonferonni correction is applied for the number of tests (columns)
-% M is the central tendency estimator of the difference
-% CI are the alpha percent (Bonferoni corrected) confidence intervals
-% p are the p values
-%
-% Cyril Pernet - v1 - April 2012
+% Cyril Pernet - v2 - March 2017
 % ----------------------------------------------------------------------
-% Copyright (C) RST Toolbox Team 2014
+% Copyright (C) RST Toolbox Team 2017
 
 %% check arguments
-est = 'trimmean';
-trimming = 20/100; % amount of trimming
-alphav = 5/100;
-nboot = 600;
-plot_option = 0;
-X = varargin{1};
-Y = varargin{2};
- 
-[n,p] = size(X);
-if n == 1 && p > 1 % if vectors are used, doesn't matter the direction
-    X = X';
-    Y = Y';
+
+% _hard coded_
+nboot = 600; % how many bootstraps are used
+
+% _soft coded_
+alphav   = 5/100; % non adjusted alpha value
+estimator ='trimmed mean'; % by default use a robust estimator
+newfig    = 'yes'; % start a new figure
+plothist  = 'no'; % do not create additional histograms of differences
+
+% _check inputs_
+if any(size(gp1,2) ~= size(gp2,2))
+    error('input data must be of the same size')
+else
+    [n,p] = size(gp1);
 end
 
-% other argument
-if nargin>=2 && nargin<7
-    if nargin >= 3; est = varargin{3}; end
-    EST{1} = 'mean'; EST{2} = 'median'; EST{3} = 'trimmean';
-    if ~ischar(est)
-        error('estimator argument must be a character')
-    elseif isempty(strmatch(est,EST))
-        error('unknown estimator name')
+% check options
+for in=1:length(varargin)
+    if strcmpi(varargin(in),'alphav')
+        alphav = cell2mat(varargin(in+1));
+    elseif strcmpi(varargin(in),'estimator')
+        if ~strcmpi(varargin(in+1),'median') && ...
+                ~strcmpi(varargin(in+1),'mean') && ...
+                ~strcmpi(varargin(in+1),'trimmed mean')
+            error(['estimator ' cell2mat(varargin(in+1)) ' is not recognized'])
+        else
+            estimator = cell2mat(varargin(in+1));
+        end
+    elseif strcmpi(varargin(in),'newfig')
+        newfig = cell2mat(varargin(in+1));
+    elseif strcmpi(varargin(in),'plothist')
+        plothist = cell2mat(varargin(in+1));
     end
-    
-    if nargin >= 4; alphav = varargin{4}; end
-    if nargin >= 5; plot_option = varargin{5}; end
-    if ~isnumeric(alphav)
-        error('alphav must be a numeric')
-    elseif alphav > 1
-        alphav = alphav / 100;
-    end
-    
-    if nargin == 6; nboot = varargin{6}; end
-    if ~isnumeric(nboot)
-        error('nboot must be a numeric')
-    elseif nboot < 600
-        error('the number of bootstraps is too small')
-    end
-else
-    error('wrong number of arguments')
+end
+orig_alpha = alphav;
+
+[n,p] = size(gp1);
+if n == 1 && p > 1 % if vectors are used, doesn't matter the direction
+    gp1 = gp1';
+    gp2 = gp2';
 end
 
 %% adjust p value if needed
-alphav = alphav / size(X,2); % Bonferoni adjustment
+alphav = alphav / size(gp1,2); % Bonferoni adjustment
+low = round(nboot.*orig_alpha./2); % CI bounds
+high = nboot - low;
 
 %% create a resampling table
 
-table1 = randi(size(X,1),size(X,1),nboot);
-table2 = randi(size(Y,1),size(Y,1),nboot);
-for c=1:size(X,2)
-        v = X(:,c);
-        data1{c} = v(table1); % applies the sample resampling for each column
-        v = Y(:,c);
-        data2{c} = v(table2); % applies the sample resampling for each column
+table1 = randi(size(gp1,1),size(gp1,1),nboot);
+table2 = randi(size(gp2,1),size(gp2,1),nboot);
+for c=1:size(gp1,2)
+    v = gp1(:,c);
+    data1{c} = v(table1); % applies the sample resampling for each column
+    v = gp2(:,c);
+    data2{c} = v(table2); % applies the sample resampling for each column
 end
 
 %% do the percentile test
 
-low = round((alphav*nboot)/2);
-high = nboot - low;
-n(1) = size(X,1);
-n(2) = size(Y,1);
+n(1) = size(gp1,1);
+n(2) = size(gp2,1);
 
 % get the difference just for output
-if strcmp(est,'mean')
-    M = nanmean(X,1) - nanmean(Y,1);
-elseif strcmp(est,'median')
-    M = nanmedian(X,1) - nanmedian(Y,1);
-elseif strcmp(est,'trimmean')
-    M = rst_trimmean(X,trimming)- rst_trimmean(Y,trimming);
+if strcmpi(estimator,'mean')
+    M = nanmean(gp1,1) - nanmean(gp2,1);
+elseif strcmpi(estimator,'median')
+    M = rst_hd(gp1,0.5) - rst_hd(gp2,0.5);
+elseif strcmpi(estimator,'trimmed mean')
+    M = rst_trimmean(gp1)- rst_trimmean(gp2);
 end
 
-for c=1:size(X,2)
-    if strcmp(est,'mean')
+for c=1:size(gp1,2)
+    if strcmpi(estimator,'mean')
         m{c} = sort(nanmean(data1{c},1) - nanmean(data2{c},1));
-    elseif strcmp(est,'median')
-        m{c} = sort(nanmedian(data1{c},1) - nanmedian(data2{c},1));
-    elseif strcmp(est,'trimmean')
-        m{c} = sort(rst_trimmean(data1{c},trimming)- rst_trimmean(data2{c},trimming));
+    elseif strcmpi(estimator,'median')
+        m{c} = sort(rst_hd(data1{c}) - rst_hd(data2{c}));
+    elseif strcmpi(estimator,'trimmed mean')
+        m{c} = sort(rst_trimmean(data1{c})- rst_trimmean(data2{c}));
     end
     
     CI(1,c) = m{c}(low);
@@ -128,48 +118,46 @@ end
 h = p<alphav;
 
 %% plot
-    
-if plot_option == 1
+
+if strcmpi(newfig,'yes')
     figure('Name', 'Two-samples percentile bootstrapt')
-    if strcmp(est,'mean')
-        ylabel('Mean diffence','FontSize',14); hold on; grid on
-        if size(X,2) ==1
-            title('Mean and 95% CI','FontSize',16); grid on
-        else
-            title(['Means and ' num2str(100-alphav*100) '% CI'],'FontSize',16); grid on
-        end
-        
-    elseif strcmp(est,'median')
-        ylabel('Median difference','FontSize',14); hold on; grid on
-        if size(X,2) ==1
-            title('Median and 95% CI','FontSize',16); grid on
-        else
-            title(['Medians and ' num2str(100-alphav*100) '% CI'],'FontSize',16); grid on
-        end
-        
-    elseif strcmp(est,'trimmean')
-        ylabel('TrimMean difference','FontSize',14); hold on; grid on
-        if size(X,2) ==1
-            title('TrimMean and 95% CI','FontSize',16); grid on
-        else
-            title(['TrimMeans and ' num2str(100-alphav*100) '% CI'],'FontSize',16); grid on
-        end
-    end
-    
-    bar([1:size(X,2)],M);
-    errorbar([1:size(X,2)],M,M-CI(1,:),M-CI(2,:),'r','LineWidth',2);
-        
-    if size(X,2) ==1
-        xlabel('Condition 1','FontSize',14);        
-    else
-        for i=1:size(X,2)
-            xname{i} = [num2str(i) ' '];
-        end
-        xlabel(sprintf('Conditions %s', cell2mat(xname)),'FontSize',14);
-    end
-    set(gca,'FontSize',12);
-    v=axis; ymin = v(3) + (v(4)-v(3))/10;
-    set(gcf,'Color','w')
+    set(gcf,'Color','w');
 end
 
+L = size(gp1,2);
+color_scheme = cubehelixmap('semi_continuous',L+10);
+if L ==1
+    title([estimator ' and 95% CI'],'FontSize',16); grid on
+else
+    title([estimator 's and ' num2str(100-alphav*100) '% CI'],'FontSize',16); grid on
+end
+ylabel([estimator ' difference'],'FontSize',14); hold on; grid on
+
+for i=1:L
+    rectangle('Position',[i-0.2,CI(1,i),0.4,CI(2,i)-CI(1,i)],'Curvature',[0.4 0.4],'LineWidth',2,...
+        'FaceColor',color_scheme(i+8,:),'EdgeColor',[0.35 0.35 0.35]); hold on;
+    plot([i-0.2 i+0.2],[M(i) M(i)],'LineWidth',3,'Color',[0.35 0.35 0.35]);
+    tmp = repmat(m{i}',1,2); tmp(1:2:end,1) = NaN; tmp(2:2:end,2) = NaN;
+    scatter(repmat(i-0.025,nboot,1),tmp(:,1),50,'k');
+    scatter(repmat(i+0.025,nboot,1),tmp(:,2),50,'k');
+end
+
+if L ==1
+    xlabel('Condition 1','FontSize',14);
+else
+    for i=1:size(gp1,2)
+        xname{i} = [num2str(i) ' '];
+    end
+    xlabel(sprintf('Differences %s', cell2mat(xname)),'FontSize',14);
+end
+v=axis; ymin = v(3) + (v(4)-v(3))/10;
+
+if strcmpi(plothist,'yes')
+    figure('Name', 'Histograms of differences')
+    set(gcf,'Color','w'); L = size(gp1,2);
+    nrow = round(L/5);
+    for i=1:L
+        subplot(nrow,5,i); rst_density_hist(m{i});
+    end
+end
 
