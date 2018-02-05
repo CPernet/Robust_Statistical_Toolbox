24014d138eac6127955732e1a72dbb42167efce9
Cyril Pernet <wamcyril@gmail.com>
2018-02-05 10:14:13 +0000

    tidying up

--- stats_functions/low_level_stats_functions/rst_wincov.m
+++ stats_functions/low_level_stats_functions/rst_wincov.m
@@ -1,17 +1,21 @@
-function wincov = rst_#wincov(Data,Percent)
+function wincov = rst_wincov(Data,Percent)
 
 % Computes the winsorized covariance between the columns of the matrix Data
 %
-% FORMAT
+% FORMAT wincov = rst_wincov(Data,Percent)
 %
-% INPUT
+% INPUT Data is a vector
+%       Percent is the anount of trimming (default is 20%)
 %
-% OUTPUT
+% OUTPUT wincov is the winsorized covariance matrix (ie the covariance of
+%        the wonsorzed data)
 %
-% Cyril Pernet June 2012
+% Cyril Pernet 
+% -------------------------------------------------------------------------
+% Copyright (C) RST Toolbox Team 2015
 
 if nargin == 1
-Percent = 20/100;
+    Percent = 20/100;
 end
 
 if Percent > 1
@@ -31,29 +35,22 @@ lower_bounds=Sorted_Data(g+1,:);
 higher_bounds=Sorted_Data(n-g,:);
 
 % find which values have to be changed
-low_outliers = NaN(g+1,size(Data,2));
-high_outliers = NaN(g+1,size(Data,2));
-
 for i=1:size(Data,2)
-    low_outliers(:,i) = Sorted_Data(find(Sorted_Data(:,i)<=repmat(lower_bounds(i),n,1)),i);
-    high_outliers(:,i) = Sorted_Data(find(Sorted_Data(:,i)>=repmat(higher_bounds(i),n,1)),i);
+    low_outliers{i}  = Sorted_Data(find(Sorted_Data(:,i)<=repmat(lower_bounds(i),n,1)),i);
+    high_outliers{i} = Sorted_Data(find(Sorted_Data(:,i)>=repmat(higher_bounds(i),n,1)),i);
 end
 
 % substitute in original data to keep the relationship and compute covariance
 WinData = Data;
 for i=1:size(Data,2)
-    for j=1:(g+1)
-        WinData(Data(:,i) == low_outliers(j,i),i) = lower_bounds(i);
-        WinData(Data(:,i) == high_outliers(j,i),i) = higher_bounds(i);
+    for j=1:length(low_outliers{i})
+        WinData(Data(:,i) == low_outliers{i}(j),i) = lower_bounds(i);
+    end
+    
+    for j=1:length(high_outliers{i})
+        WinData(Data(:,i) == high_outliers{i}(j),i) = higher_bounds(i);
     end
 end
 
-% % we can check it works
-% Sorted_WinData=NaN(size(Data));
-% for i=1:size(Data,2)
-%     Sorted_WinData(:,i) = sort(squeeze(WinData(:,i)));
-% end
-% [Sorted_Data Sorted_WinData] 
- 
 wincov = cov(WinData);
 
